5f7c2030a30012036cac3153b6f1e8f204d834df
Timothy Perera <timothyperera.m@gmail.com>
2024-07-24 23:01:10 -0400

    add logger config file

--- data_import.py
+++ data_import.py
@@ -1,14 +1,32 @@
+import logging.config
 import requests
 import pandas as pd
 import zipfile
 from io import BytesIO
 import re
 import os
+import logging
+from dotenv import load_dotenv
+import yaml
+
+load_dotenv()
+
+def setupLogging():
+    with open('log_configuration.yaml','r') as log_config:
+        config = yaml.safe_load(log_config)
+        logging.config.dictConfig(config)
+
+setupLogging()
+logger = logging.getLogger('basic')
+logger.info('Logger initiated.')
+
 
 OUTPUT_PATH = 'imported_data'
 
-
 def import_ridership_data(data_package):
+    """
+    Imports ridership data. Returns a list of all the file names imported.
+    """
     for resource in data_package['result']['resources']:
         link = resource['url']
         format = resource['format'].lower()
@@ -34,8 +52,9 @@ def import_ridership_data(data_package):
             print('Missing file handler for type',format)
 
 
-def get_station_data():
-    pass
+def import_station_data(station_data_pkg):
+    station_data = station_data_pkg['data']['stations']
+    pd.DataFrame.from_dict(station_data).to_csv(OUTPUT_PATH+'/station_info.csv')
 
 def get_data_package(url, params=None):
     "Handles URL requests. Returns JSON object."
@@ -52,7 +71,8 @@ def main():
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
         print('Created data directory.')
-
+    
+    # Supply URLs to extract data from
     rdata_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show"
     params = {"id": "bike-share-toronto-ridership-data"}
     sdata_url = "https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information"
@@ -61,29 +81,11 @@ def main():
     rider_data_pkg = get_data_package(rdata_url, params)
     station_data_pkg = get_data_package(sdata_url)
     import_ridership_data(rider_data_pkg)
+    import_station_data(station_data_pkg)
 
-    station_data = station_data_pkg['data']['stations']
-    pd.DataFrame.from_dict(station_data).to_csv(OUTPUT_PATH+'/station_info.csv')
+    # Create master ridership dataset
     
-
-
         
-        
-# Get station information 
-# url = "https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information"
-# url ="https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/help_show?name=help_show"
-
-# response = requests.get(url)
-# data = response.json()
-# print(data['result'])
-# # station_info = data['data']
-
-# print(len(station_info['stations']))
-
-# data = pd.DataFrame.from_dict(station_info['stations'])
-# data.to_csv('station_data.csv')
-# #print(station_info)
-
 
 if __name__=='__main__':
     main()
